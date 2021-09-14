3421f0910a0b196e0f04c8dfc7b3fd06d1869ca8
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 04:08:58 +0530

    Welcome On Binary Tree Program

---
+++ src/com/bl/mybinarytree/MyBinaryTreeMain.java
@@ -0,0 +1,15 @@
+package com.bl.mybinarytree;
+
+public class MyBinaryTreeMain {
+	public static void main(String[] args) {
+
+		System.out.println("------ Welcome to Data Structure : Binary Search Tree ------");
+
+		/*
+		 * MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		 * myBinaryTree.add(56); myBinaryTree.add(30); myBinaryTree.add(70);
+		 * 
+		 * myBinaryTree.printBinaryTree();
+		 */
+	}
+}
---
+++ src/com/bl/mybinarytree/package-info.java
@@ -0,0 +1,1 @@
+package com.bl.mybinarytree;
