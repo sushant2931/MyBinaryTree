780b60cada270b2ec17892152abc3b93d6cd70ec
sushant2931 <sushantlad8888@gmail.com>
2021-09-15 04:24:15 +0530

    Added: Impliment Search Method Find 63

--- src/com/bl/mybinarytree/MyBinaryTree.java
+++ src/com/bl/mybinarytree/MyBinaryTree.java
@@ -26,6 +26,21 @@ public class MyBinaryTree<K extends Comparable<K>> {
 		this.root = this.addRecursively(root, key);
 	}
 
+	private boolean searchTreeRecursively(MyBinaryNode<K> current, K key) {
+		if (current == null) {
+			return false;
+		}
+		int compareResult = key.compareTo(current.key);
+		if (compareResult == 0) {
+			return true;
+		}
+		return compareResult < 0 ? searchTreeRecursively(current.left, key) : searchTreeRecursively(current.right, key);
+	}
+
+	public boolean searchTree(K key) {
+		return searchTreeRecursively(root, key);
+	}
+
 	private int getSizeRecursively(MyBinaryNode<K> current) {
 		return current == null ? 0 : 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
 	}
